333980ef33e32ac9722cd2640e5e51e3d96f957c
Leiza Girfatova <leiza-rad@yandex.ru>
2021-10-05 07:20:47 +0300

    commit2

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         // TODO: подставлять номер карты нужно сюда между двойными кавычками, без пробелов
-        String number = "30075084714498";
+        String number = "3545246539165025141";
         System.out.println(String.format("Result is %s", isValidCardNumber(number) ? "OK" : "FAIL"));
     }
 
